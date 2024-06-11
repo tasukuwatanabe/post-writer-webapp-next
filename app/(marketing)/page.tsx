4110cedb6412a96ee18dec9ba70087c295db7080
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-06-11 23:33:54 +0900

    Add config and types directory. Move common data

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,5 @@
 import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
@@ -8,8 +9,10 @@ export default function IndexPage() {
       <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12">
         <div className="container text-center flex flex-col items-center gap-4 max-w-[64rem]">
           <Link
-            href="/"
+            href={siteConfig.links.x}
             className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm"
+            target="_blank"
+            rel="noreferrer"
           >
             Xをフォローする
           </Link>
@@ -25,7 +28,7 @@ export default function IndexPage() {
               はじめる
             </Link>
             <Link
-              href="/"
+              href={siteConfig.links.github}
               className={cn(buttonVariants({ size: "lg", variant: "outline" }))}
               target="_blank"
               rel="noreferrer"
