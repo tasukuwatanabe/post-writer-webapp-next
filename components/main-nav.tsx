c52506a318ed5ef301ddb4be3ce6d728fc076a0d
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-06-13 23:56:18 +0900

    Develop mobile-nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -6,12 +6,12 @@ import { ReactNode, useState } from "react";
 import MobileNav from "./mobile-nav";
 
 interface MainNavProps {
-  items?: NavItem[];
+  items: NavItem[];
   children?: ReactNode;
 }
 
 export default function MainNav({ items }: MainNavProps) {
-  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(true);
+  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
 
   return <div className="flex items-center md:gap-10">
     <Link href="/" className="hidden md:flex items-center space-x-2"><span className="font-bold hidden sm:inline-block">Post Writer</span></Link>
@@ -23,6 +23,6 @@ export default function MainNav({ items }: MainNavProps) {
     <button className="md:hidden" onClick={() => setShowMobileMenu(!showMobileMenu)}>
       <span>メニュー</span>
     </button>
-    {showMobileMenu && <MobileNav />}
+    {showMobileMenu && <MobileNav items={items} />}
   </div>
 }
